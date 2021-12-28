98bad4399eb7cb65bb1bab64a796838ee87d82f9
Tom Yandell <tom@tomyandell.co>
2021-12-28 16:24:23 +0000

    Cypress tests

--- cypress/integration/lines_spec.ts
+++ cypress/integration/lines_spec.ts
@@ -1,18 +1,8 @@
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-const waitTestReady = async () => {
-    const window = await cy.window()
-    for (let i = 0; i < 100; i++) {
-        if (window.testReady) {
-            return
-        }
-        await sleep(100)
-    }
-    throw new Error('not test ready after twenty iterations')
-}
+beforeEach(() => {
+    cy.visit('/lines')
+    cy.window().should('have.property', 'appStarted')
+  })
 
 it('should display lines', async () => {
-    cy.visit('/lines');
-    await waitTestReady()
     cy.compareSnapshot('lines', 0);
 });
--- src/routes/lines.svelte
+++ src/routes/lines.svelte
@@ -6,15 +6,15 @@
     import Stage from '$lib/Stage.svelte'
     import Layer from '$lib/Layer.svelte'
     import Line from '$lib/Line.svelte'
-    globalThis.testReady = false
 </script>
 
 <Stage width={100} height={100}>
     <Layer>
         <Line x={10} y={10} points={[0, 0, 20, 20, 20, 0]} strokeWidth={2} stroke="blue"
             on:added={ () => {
-                console.log('setting test ready')
-                globalThis.testReady = true
+                if (globalThis.Cypress) {
+                    globalThis.appStarted = true
+                }
             } }
         ></Line>
     </Layer>
